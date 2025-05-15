894865f352a3ca6790e21057ef6ef83c13522602
joel alexander <wksx9d@gmail.com>
2025-05-14 23:28:33 -0500

    feat(data): agrega interfaz DAO para tareas

---
+++ app/src/main/java/com/example/lab08/data/dao/TaskDao.kt
@@ -0,0 +1,28 @@
+package com.example.lab08.data.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.lab08.data.model.Task
+
+
+@Dao
+interface TaskDao {
+    //obtener todas las tareas
+    @Query("SELECT * FROM tasks")
+    suspend fun getAllTasks(): List<Task>
+
+    //insertar  una nueva tarea
+    @Insert
+    suspend fun insertTasks(task: Task)
+
+    //marcar una tarea como terminado
+    @Update
+    suspend fun updateTask(task: Task)
+
+    //eliminar una tarea
+    @Query("DELETE FROM tasks")
+    suspend fun deleteALLTasks()
+
+}
